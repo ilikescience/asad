32e9d5b3d960c193d1a31e49fde0e5c45f6d79b7
Matt Strom <matthew.h.strom@gmail.com>
2015-03-30 09:58:20 -0400

    formatted the form, got compilation and copying working.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,71 @@
 module.exports = {};
+
+var ZeroClipboard = require( 'zeroclipboard' );
+
+var getBitly = function(link) {
+    var token = "748c81597b1afe6ae5a27a37d19a88bdc7bc7275";
+    var longLink = encodeURIComponent(link);
+    var bitlyURL = "https://api-ssl.bitly.com/v3/shorten?access_token=" + token + "&longUrl=" + longLink;
+
+    $.getJSON( bitlyURL , function( data ) {
+        var newURL = data.data.url;
+        $('.tweet--url').html(newURL);
+        updateTweetStatus();
+    });
+};
+
+var updateTweetStatus = function() {
+    var tweetLength = $('.input--tweet').text().length;
+    $('.tweet--chars').text(tweetLength);
+    var charsStatus = (tweetLength > 140 ? "bad" : "good");
+    $('.tweet--status').attr('class', 'tweet--status status--' + charsStatus);
+};
+
+var compile = function() {
+    $(".result--url").html($(".tweet--url").html());
+    $(".result--songname").html($(".input--songname").val());
+    $(".result--artist").html($(".input--artist").val());
+    tweetLink();
+};
+
+var tweetLink = function() {
+    var status = $('.input--tweet').text();
+    var URIstatus = encodeURIComponent(status);
+    $('.result--tweet').html('http://twitter.com/home/?status=' + URIstatus);
+};
+
+$(document).ready( function () {
+
+    var client = new ZeroClipboard( document.getElementById("copy-button") );
+
+    client.on( "ready", function( readyEvent ) {
+
+      client.on( "copy", function (event) {
+        compile();
+        var clipboard = event.clipboardData;
+        var thisVal = $('.result').html();
+        clipboard.setData( "text/html", thisVal );
+      });
+
+      client.on( "aftercopy", function( event ) {
+        $(event.target).addClass('success');
+        $(event.target).html('&#10004; Copied to clipboard!');
+      } );
+    } );
+
+    $('.input--url').on('keyup', function(){
+        var thisUrl = $(this).val();
+        console.log('getting shortlink for ' + thisUrl);
+        getBitly(thisUrl);
+    });
+
+    $('.input').on('keyup', function(){
+        updateTweetStatus();
+
+        var thisTarget = $(this).attr('id');
+        var thisVal = $(this).val();
+
+        $('.tweet--' + thisTarget).html(thisVal);
+    });
+
+});
